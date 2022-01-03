43930427e4d2db4e6c2aee3ef747aa39a6ed27ea
PAUL TANGBAOAN <paulchristian_tangbaoan@yahoo.com>
2022-01-03 20:08:52 +0800

    Add files via upload

---
+++ Handlers/ExceptionHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace TVMazeWebAPIApp.Handlers
+{
+    /// <summary>
+    /// Defines the <see cref="ExceptionHandler" />.
+    /// </summary>
+    public class ExceptionHandler
+    {
+        /// <summary>
+        /// Defines the next.
+        /// </summary>
+        private readonly RequestDelegate next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionHandler"/> class.
+        /// </summary>
+        /// <param name="next">The next<see cref="RequestDelegate"/>.</param>
+        public ExceptionHandler(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        /// The Invoke.
+        /// </summary>
+        /// <param name="context">The context<see cref="HttpContext"/>.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        public async Task Invoke(HttpContext context)
+        {
+
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/text";
+                await context.Response.WriteAsync(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Interfaces/IShowRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TVMazeWebAPIApp.Interfaces
+{
+    public interface IShowRepository
+    {
+        Task<DataTable> GetShowWithCast(int pageStart, int pageEnd);
+        Task<DataTable> GetShowWithCast(int id);
+
+    }
+}
